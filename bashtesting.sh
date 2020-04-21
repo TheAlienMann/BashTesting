549a0bed5c09a52533c01424968f9f03779f52b6
TheAlienMann <rahmanipour.mohammad@gmail.com>
2020-04-21 09:22:48 +0430

    Added comment on find command

---
+++ bashtesting.sh
@@ -0,0 +1,9 @@
+# find . -name "TheFileYouAreLookingFor"
+# the dot, determines the current folder or directory that you arein.
+# to give find a path
+# find /path/of/the/file/youAreLookingFor -name "theName"
+# -empty flag shows the empty files
+# -iname flag, case insencitive search for the file with the igven name.
+# there is also wild card taht you can use to search
+# like find . -iname "*.swift"
+# the line above shows all th files that have swift extension.
